77335cc474aa5f45d918314a139e96219876e5d9
Abhinav Sonkar <xsabhinav@gmail.com>
2020-04-26 10:24:12 +0200

    Import statewise testing data as well

--- covid19-importer.py
+++ covid19-importer.py
@@ -18,23 +18,25 @@ def acked(err, msg):
 def lambda_handler(event, context):
     covid19_api_raw_data_url = os.getenv('COVID19_API_RAW_DATA_URL')
     covid19_api_state_data_url = os.getenv('COVID19_API_STATE_DATA_URL')
+    covid19_api_test_data_url = os.getenv('COVID19_API_TEST_DATA_URL')
     bootstrap_servers = os.getenv('BOOTSTRAP_SERVERS')
     kafka_client_id = os.getenv('KAFKA_CLIENT_ID')
     kafka_patient_data_topic_name = os.getenv('KAFKA_PATIENT_DATA_TOPIC_NAME')
     kafka_state_data_topic_name = os.getenv('KAFKA_STATE_DATA_TOPIC_NAME')
+    kafka_test_data_topic_name = os.getenv('KAFKA_TEST_DATA_TOPIC_NAME')
     telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
     telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-
-    bot = Bot(token=telegram_bot_token)
-
-    resp = requests.get(url=covid19_api_raw_data_url)
-    data = resp.json()
 
     conf = {'bootstrap.servers': bootstrap_servers,
             'client.id': kafka_client_id,
             'linger.ms': '1000'}
 
     producer = Producer(conf, logger=logger)
+
+    bot = Bot(token=telegram_bot_token)
+
+    resp = requests.get(url=covid19_api_raw_data_url)
+    data = resp.json()
 
     for p in data['raw_data']:
         try:
@@ -73,6 +75,34 @@ def lambda_handler(event, context):
 
     bot.send_message(chat_id=telegram_chat_id,
                      text='Imported {} states data into Kafka'.format(len(state_data_json['statewise'])))
+
+    resp = requests.get(url=covid19_api_test_data_url)
+    data = resp.json()
+
+    test_data_count = 0
+    for p in data['states_tested_data']:
+        try:
+            if p['totaltested'] == '':
+                continue
+            key = dict({u'state': p['state'], u'date': p['updatedon']})
+            producer.produce(topic=kafka_test_data_topic_name, value=json.dumps(p), key=json.dumps(key),
+                             on_delivery=acked)
+            test_data_count += 1
+        except BufferError:
+            logger.error('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                         len(producer))
+
+        # Serve delivery callback queue.
+        # NOTE: Since produce() is an asynchronous API this poll() call
+        #       will most likely not serve the delivery callback for the
+        #       last produce()d message.
+        producer.poll(0)
+
+    # Wait until all messages have been delivered
+    logger.info('%% Waiting for %d deliveries\n' % len(producer))
+    producer.flush()
+
+    bot.send_message(chat_id=telegram_chat_id, text='Imported {} testing data into Kafka'.format(test_data_count))
 
 
 if __name__ == '__main__':
